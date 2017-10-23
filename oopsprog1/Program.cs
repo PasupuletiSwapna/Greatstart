fd3370c3ec56a616f23e00a23f3ec482baa085cf
swapna <pasupuletisaiswapna85@gmail.com>
2017-10-23 16:24:28 +0200

    oopsprogupdate

--- oopsprog1/Program.cs
+++ oopsprog1/Program.cs
@@ -13,16 +13,48 @@ namespace oopsprog1
 
         static void Main(string[] args)
         {
-            var e1 = new Employee("Sai");
-            e1.Salary = 25000;
+            //var e1 = new Employee("Sai");
+            //e1.Salary = 25000;
 
-            var e2 = new Employee("Ram");
-            e2.Salary = 23000;
-          
+            //var e2 = new Employee("Ram");
+            //e2.Salary = 23000;
+
             var registry = new Registry();
-            registry.Add(e1);
-            registry.Add(e2);
+            var added = true;
+            do {
+                added = AddEmployee(registry);
+            } while (added);
+            Console.WriteLine("=============");
+
             registry.List();
         }
+        private static bool AddEmployee(Registry registry)
+        { 
+            Console.Write("Name: ");
+                var name = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(name))
+                return false;
+
+                  Console.Write("Salary: ");
+
+                var salarystring = Console.ReadLine();
+                int salary = 0;
+                if (!int.TryParse(salarystring, out salary))
+                { 
+                        Console.WriteLine("salary couldnt be parsed");
+                    }
+                    var employee = new Employee(name);
+                    employee.Salary = salary;
+
+                    registry.Add(employee);
+                    return true;
+
+
+               
+                
+            }
     }
-}
+    }
+
+
