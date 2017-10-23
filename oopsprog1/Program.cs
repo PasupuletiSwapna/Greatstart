42dba2c7d20a03a7380869e984ab8ed21488efac
swapna <pasupuletisaiswapna85@gmail.com>
2017-10-23 15:24:24 +0200

    oopsprog1

---
+++ oopsprog1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsprog1
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            var e1 = new Employee("Sai");
+            e1.Salary = 25000;
+
+            var e2 = new Employee("Ram");
+            e2.Salary = 23000;
+          
+            var registry = new Registry();
+            registry.Add(e1);
+            registry.Add(e2);
+            registry.List();
+        }
+    }
+}
---
+++ oopsprog1/Registry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsprog1
+{
+    class Registry
+    {
+
+        private List<Employee> list = new List<Employee>();
+        public void Add(Employee employee)
+        {
+            //...
+            list.Add(employee);
+        }
+
+        public void List()
+        {
+            //...
+            foreach (var employee in list)
+            {
+                Console.WriteLine("Name:" + employee.Name);
+                Console.WriteLine("Salary:" + employee.Salary);
+                Console.WriteLine();
+            }
+        }
+    }
+}
